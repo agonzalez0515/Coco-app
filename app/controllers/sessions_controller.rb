1cceb1c525f6d40847c4627fb8895b7851eb6ea2
Zara Aslam <zaslam72@gmail.com>
2016-12-19 18:15:59 -0800

    update users and sessions view pages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:email])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect_to user_path
+      redirect_to user_path(@user)
     else
       @errors = ["Email or password is incorrect"]
       render 'new'
