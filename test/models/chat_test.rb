143be67022fb2991ccf658520c789f2b819f299e
Techforchange <arleneperezsb@gmail.com>
2016-12-20 11:30:46 -0800

    Minimized google map for SAT locations

--- test/models/chat_test.rb
+++ test/models/chat_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: chats
+#
+#  id         :integer          not null, primary key
+#  body       :text
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class ChatTest < ActiveSupport::TestCase
