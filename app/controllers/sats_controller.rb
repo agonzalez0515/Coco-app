f0058ebed221403bd3b9090800d11a5373c13b74
agonzalez0515 <agonzalez0515@gmail.com>
2016-12-19 20:39:44 -0800

    add map functionality

--- app/controllers/sats_controller.rb
+++ app/controllers/sats_controller.rb
@@ -1,5 +1,11 @@
 class SatsController < ApplicationController
+
   def index
+    @sats = Sat.where(date: Date.parse(params[:date]))
+    puts "Found #{@sats.size} SATs on #{@sats.map(&:date).uniq}"
+    respond_to do |format|
+      format.js {render json: @sats.map(&:attributes)}
+    end
   end
 
 end
