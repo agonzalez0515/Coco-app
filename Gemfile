6496e3eaf62bfc48cee4a6c343eaab7a468132c7
agonzalez0515 <agonzalez0515@gmail.com>
2016-12-28 14:56:15 -0800

    add factory girl gem

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ group :development, :test do
   gem 'rake'
   gem 'rspec-rails'
   gem 'capybara'
+  gem "factory_girl_rails", "~> 4.0"
 end
 
 group :test do
--- spec/models/chat_spec.rb
+++ spec/models/chat_spec.rb
@@ -3,4 +3,8 @@ require 'rails_helper'
 RSpec.describe Chat, type: :model do
     it { should belong_to(:user) }
     it { should validate_presence_of (:body) }
+
+    
+
+    expect(chat.for_display).to eq
 end
