7db1799836916cbf9cc393446e811e3a3164b4b4
Robert Turner Jr <georob23@gmail.com>
2017-01-24 20:07:16 -0800

    Add Tag form.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,12 +1,18 @@
 class TagsController < ApplicationController
 
+  protect_from_forgery
+
   def new
     @tag = Tag.new
   end
 
   def create
     @user = current_user
+    @message = Message.find(params[:tag][:message_id])
     @tag = Tag.new(tag_params)
+    # if @message.tags.where(title: params[:tag][:title]).length > 1
+    #   @tag.count += 1
+    # end
     if @tag.save
       redirect_to Message.find(params[:tag][:message_id])
     else
@@ -16,8 +22,8 @@ class TagsController < ApplicationController
   end
 
 private
-  def message_params
-    message_params = { title: params[:tag][:title],
+  def tag_params
+    tag_params = { title: params[:tag][:title],
                      message_id: params[:tag][:message_id],
                      count: params[:tag][:count] }
   end
