7860041634a433560739e25d5228ee317e5a3a4e
Techforchange <arleneperezsb@gmail.com>
2017-02-03 15:24:00 -0800

    Deletes +1 from phone contact in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,102 +1,15 @@
 
-# require 'open-uri'
-# Scraping for SAT locations
-# page1 = Nokogiri::HTML(open("https://collegereadiness.collegeboard.org/api/v1/test-center-search?test_date=201701&country=US&state=CA&city="))
-
-#  january_nokogiri = page1.css('div.test-center-results-row')
-#  january_locations = []
-
-
-# require 'open-uri'
-# #Scraping for SAT locations
-# page1 = Nokogiri::HTML(open("https://collegereadiness.collegeboard.org/api/v1/test-center-search?test_date=201701&country=US&state=CA&city="))
-#
-#  january_nokogiri = page1.css('div.test-center-results-row')
-#  january_locations = []
-
-#  january_nokogiri.each do |school|
-#    schools = {}
-#    schools[:name] = school.css('h3').text
-#    schools[:address] = school.css('.test-center-result-value').text.split(" ")[0...-1].join(' ')
-#    january_locations << schools
-#  end
-
-#  january_locations.each do |school|
-#    Sat.create!(location_name: school[:name], address: school[:address], date: Date.parse('2017-01-21') )
-#  end
-
-#  january_locations.each do |school|
-#    Sat.create!(location_name: school[:name], address: school[:address], date: Date.parse('2017-01-21') )
-#  end
-
-# ################################################################################
-# page2 = Nokogiri::HTML(open("https://collegereadiness.collegeboard.org/api/v1/test-center-search?test_date=201703&country=US&state=CA&city="))
-# march_nokogiri = page2.css('div.test-center-results-row')
-# march_locations = []
-
-# march_nokogiri.each do |school|
-#   schools = {}
-#   schools[:name] = school.css('h3').text
-#   schools[:address] = school.css('.test-center-result-value').text.split(" ")[0...-1].join(' ')
-#   march_locations << schools
-# end
-
-# march_locations.each do |school|
-#   Sat.create!(location_name: school[:name], address: school[:address], date: Date.parse('2017-03-11') )
-# end
-# ################################################################################
-# page3 = Nokogiri::HTML(open("https://collegereadiness.collegeboard.org/api/v1/test-center-search?test_date=201705&country=US&state=CA&city="))
-# may_nokogiri = page3.css('div.test-center-results-row')
-# may_locations = []
-
-# may_nokogiri.each do |school|
-#   schools = {}
-#   schools[:name] = school.css('h3').text
-#   schools[:address] = school.css('.test-center-result-value').text.split(" ")[0...-1].join(' ')
-#   may_locations << schools
-# end
-
-# may_locations.each do |school|
-#   Sat.create!(location_name: school[:name], address: school[:address], date: Date.parse('2017-05-06') )
-# end
-
-# may_locations.each do |school|
-#   Sat.create!(location_name: school[:name], address: school[:address], date: Date.parse('2017-05-06') )
-# end
-
-# ################################################################################
-# page4 = Nokogiri::HTML(open("https://collegereadiness.collegeboard.org/api/v1/test-center-search?test_date=201706&country=US&state=CA&city="))
-# june_nokogiri = page4.css('div.test-center-results-row')
-# june_locations = []
-
-# june_nokogiri.each do |school|
-#   schools = {}
-#   schools[:name] = school.css('h3').text
-#   schools[:address] = school.css('.test-center-result-value').text.split(" ")[0...-1].join(' ')
-#   june_locations << schools
-# end
-
-# june_locations.each do |school|
-#   Sat.create!(location_name: school[:name], address: school[:address], date: Date.parse('2017-06-03') )
-# end
-
-#
-# june_locations.each do |school|
-#   Sat.create!(location_name: school[:name], address: school[:address], date: Date.parse('2017-06-03') )
-# end
-#
-
 # ################################################################################
 
-zara = User.create!(first_name:"Zara", last_name: "Aslam", email:"zara@zara.com", password: "password", phone_number: "+15105579120", street_address: "221 7th Street", city:"San Francisco", state: "CA", zip_code: 94105, lang_preference: "spanish", grade_level: 9, user_type: "parent")
+zara = User.create!(first_name:"Zara", last_name: "Aslam", email:"zara@zara.com", password: "password", phone_number: "5105579120", street_address: "221 7th Street", city:"San Francisco", state: "CA", zip_code: 94105, lang_preference: "spanish", grade_level: 9, user_type: "parent")
 
-rob = User.create!(first_name:"Rob", last_name: "Turner", email:"rob@rob.com", password: "password", phone_number: "+19252474748", street_address: "3259 Madera Avenue", city:"San Francisco", state: "CA", zip_code: 94619, lang_preference: "english", grade_level: 10, user_type: "parent")
+rob = User.create!(first_name:"Rob", last_name: "Turner", email:"rob@rob.com", password: "password", phone_number: "9252474748", street_address: "3259 Madera Avenue", city:"San Francisco", state: "CA", zip_code: 94619, lang_preference: "english", grade_level: 10, user_type: "parent")
 
-julia = User.create!(first_name:"Julia", last_name: "Yulak", email:"julia@julia.com", password: "password", phone_number: "+14155188406", street_address: "8735 Holbrook Street", city:"Los Angeles", state: "CA", zip_code: 90660, lang_preference: "spanish", grade_level: 10, user_type: "parent")
+julia = User.create!(first_name:"Julia", last_name: "Yulak", email:"julia@julia.com", password: "password", phone_number: "4155188406", street_address: "8735 Holbrook Street", city:"Los Angeles", state: "CA", zip_code: 90660, lang_preference: "spanish", grade_level: 10, user_type: "parent")
 
-angie = User.create!(first_name:"Angie", last_name: "Gonzalez", email:"angie@angie.com", password: "password", phone_number: "+13102920098", street_address: "2543 Congress Street", city:"San Diego", state: "CA", zip_code: 92110, lang_preference: "spanish", grade_level: 12, user_type: "parent")
+angie = User.create!(first_name:"Angie", last_name: "Gonzalez", email:"angie@angie.com", password: "password", phone_number: "3102920098", street_address: "2543 Congress Street", city:"San Diego", state: "CA", zip_code: 92110, lang_preference: "spanish", grade_level: 12, user_type: "parent")
 
-arlene = User.create!(first_name:"Arlene", last_name: "Perez", email:"arlene@arlene.com", password: "password", phone_number: "+15623221545", street_address: "6504 Pardall Rd", city:"Isla Vista", state: "CA", zip_code: 93117, lang_preference: "spanish", grade_level: 11, user_type: "student")
+arlene = User.create!(first_name:"Arlene", last_name: "Perez", email:"arlene@arlene.com", password: "password", phone_number: "5623221545", street_address: "6504 Pardall Rd", city:"Isla Vista", state: "CA", zip_code: 93117, lang_preference: "spanish", grade_level: 11, user_type: "student")
 
 ################################################################################
 chat1 = Chat.create!(body:"What is an AP course?", user_id: julia.id)
@@ -191,9 +104,3 @@ school_csv.each do |school|
               latitude:school[4].to_f,
               longitude:school[5].to_f})
             end
-
-# ################################################################################
-#Creates randomized events for practice website
-# 25.times do
-#   Event.create!(user_id: rand(7), sat_id: rand(100))
-# end
